18483ff54d782cdbba2d5c8cf3ce78e63c903718
egetelli <egetelli@hotmail.com>
2022-12-19 10:46:53 +0300

    login ve register eklemeler

--- src/assets/components/Login.jsx
+++ src/assets/components/Login.jsx
@@ -1,6 +1,8 @@
 import Navbar from './Navbar'
 import React, { useState } from "react";
 import ParticlesBg from "particles-bg";
+import { Link } from 'react-router-dom';
+
 
 export const Login = (props) => {
     const [email, setEmail] = useState('');
@@ -26,7 +28,8 @@ export const Login = (props) => {
                   <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
                   <button class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900" type="submit">Log In</button>
               </form>
-              <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
+              <Link class="underline" to="/Register">Don't have an account? Register here.</Link>
+
           </div>
         </div>
       </div>
--- src/assets/components/Register.jsx
+++ src/assets/components/Register.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Navbar from './Navbar'
 import ParticlesBg from "particles-bg";
+import { Link } from 'react-router-dom';
+
 
 export const Register = (props) => {
     const [email, setEmail] = useState('');
@@ -30,8 +32,7 @@ export const Register = (props) => {
               <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
               <button class="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900" id="button" type="submit">Submit</button>
           </form>
-          <button className="link-btn" onClick={() => props.onFormSwitch('./Login')}>Already have an account? Login here.</button>
-          <Link to="/Login">Already have an account? Login here.</Link>
+          <Link class="underline" to="/Login">Already have an account? Login here.</Link>
           </div>
         </div>
       </div>
